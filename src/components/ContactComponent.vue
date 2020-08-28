54e24eb4067fb6e4de16ec8a9642eca679ff6abd
Jogesh Muppala <jmuppala@hotmail.com>
2020-08-28 21:03:26 +0800

    Vuelidate Form Validation

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -29,27 +29,37 @@
         <form novalidate @submit.prevent="submitFeedback">
             <div class="md-layout md-gutter">
                 <div class="md-layout-item md-small-size-100 md-size-45">
-                    <md-field>
+                    <md-field :class="getValidationClass('firstname')">
                         <label for="firstname">First Name</label>
-                        <md-input name="firstname" id="firstname" autocomplete="firstname" v-model="feedback.firstName" />
+                        <md-input name="firstname" id="firstname" autocomplete="firstname" v-model.trim="$v.feedback.firstname.$model" />
+                        <span class="md-error" v-if="!$v.feedback.firstname.required">First name is required</span>
+                        <span class="md-error" v-else-if="!$v.feedback.firstname.minLength">First name must be at least {{$v.feedback.firstname.$params.minLength.min}} characters</span>
+                        <span class="md-error" v-else-if="!$v.feedback.firstname.maxLength">First name must be less than {{$v.feedback.firstname.$params.maxLength.max}} characters</span>
                     </md-field>
                 </div>
                 <div class="md-layout-item md-small-size-100 md-size-45">
-                    <md-field>
+                    <md-field :class="getValidationClass('lastname')">
                         <label for="lastname">Last Name</label>
-                        <md-input name="lastname" id="lastname" autocomplete="lastname" v-model="feedback.lastName" />
+                        <md-input name="lastname" id="lastname" autocomplete="lastname" v-model.trim="$v.feedback.lastname.$model" />
+                        <span class="md-error" v-if="!$v.feedback.lastname.required">Last name is required</span>
+                        <span class="md-error" v-else-if="!$v.feedback.lastname.minLength">Last name must be at least {{$v.feedback.lastname.$params.minLength.min}} characters</span>
+                        <span class="md-error" v-else-if="!$v.feedback.lastname.maxLength">Last name must be less than {{$v.feedback.lastname.$params.maxLength.max}} characters</span>
                     </md-field>
                 </div>
                 <div class="md-layout-item md-small-size-100 md-size-45">
-                    <md-field>
+                    <md-field :class="getValidationClass('telnum')">
                         <label for="telnum">Tel. Number</label>
-                        <md-input type="telnum" name="telnum" id="telnum" autocomplete="telnum" v-model="feedback.telNum" />
+                        <md-input type="telnum" name="telnum" id="telnum" autocomplete="telnum" v-model="$v.feedback.telnum.$model" />
+                        <span class="md-error" v-if="!$v.feedback.telnum.required">Tel. number is required</span>
+                        <span class="md-error" v-else-if="!$v.feedback.telnum.numeric">Only numbers accepted</span>
                     </md-field>
                 </div>
                 <div class="md-layout-item md-small-size-100 md-size-45">
-                    <md-field>
+                    <md-field :class="getValidationClass('email')">
                         <label for="email">Email</label>
-                        <md-input name="email" id="email" autocomplete="email" v-model="feedback.email" />
+                        <md-input name="email" id="email" autocomplete="email" v-model="$v.feedback.email.$model" />
+                        <span class="md-error" v-if="!$v.feedback.email.required">Email is required</span>
+                        <span class="md-error" v-else-if="!$v.feedback.email.email">Invalid email format</span>
                     </md-field>
                 </div>
                 <div class="md-layout-item md-small-size-100 md-size-45">
@@ -72,7 +82,7 @@
                     </md-field>
                 </div>
                 <div class="md-layout-item md-size-100">
-                    <md-button type="submit" class="md-primary md-raised">Submit</md-button>
+                    <md-button type="submit" class="md-primary md-raised" :disabled="this.$v.feedback.$invalid">Submit</md-button>
                 </div>
             </div>
         </form>
@@ -81,30 +91,71 @@
 </template>
 
 <script>
+  import { validationMixin } from 'vuelidate'
+  import {
+    required,
+    email,
+    numeric,
+    minLength,
+    maxLength
+  } from 'vuelidate/lib/validators'
+
 export default {
-  name: 'Contact',
+    name: 'Contact',
+    mixins: [validationMixin],
     data: () => ({
         feedback: {
-            firstName: null,
-            lastName: null,
-            telNum: null,
+            firstname: null,
+            lastname: null,
+            telnum: null,
             email: null,
             agree: false,
             contacttype: null,
             message: null
         },
     }),
+    validations: {
+      feedback: {
+        firstname: {
+          required,
+          minLength: minLength(3),
+          maxLength: maxLength(25)
+        },
+        lastname: {
+          required,
+          minLength: minLength(3),
+          maxLength: maxLength(25)
+        },
+        telnum: {
+          required,
+          numeric
+        },
+        email: {
+          required,
+          email
+        }
+      }
+    },
     methods: {
+        getValidationClass (fieldName) {
+            const field = this.$v.feedback[fieldName]
+
+            if (field) {
+                return { 'md-invalid': field.$invalid && field.$dirty }
+            }
+        },
         clearForm () {
-            this.feedback.firstName = null;
-            this.feedback.lastName = null;
-            this.feedback.telNum = null;
+            this.feedback.firstname = null;
+            this.feedback.lastname = null;
+            this.feedback.telnum = null;
             this.feedback.email = null;    
             this.feedback.agree = false;    
             this.feedback.contacttype = null;    
             this.feedback.message = null;    
+            this.$nextTick(() => { this.$v.feedback.$reset() })
         },
         submitFeedback () {
+            this.$v.feedback.$touch
             console.log(JSON.stringify(this.feedback));
             this.clearForm();
         }
